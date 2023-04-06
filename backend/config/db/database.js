348892ee8711290a59ce90ad20700a521feb33b4
Abimael <a.bimael2000@hotmail.com>
2023-04-05 23:40:39 -0300

    config dotenv, and connection with database

--- backend/config/db/database.js
+++ backend/config/db/database.js
@@ -1,9 +1,14 @@
+import dotenv from 'dotenv'
+dotenv.config();
 import Sequelize from 'sequelize'
 //import dbconfig from '../../dbconfig.js'
 const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{ 
   host: process.env.HOST, 
   dialect: 'mysql',
+  port: 3307
 })
+
+console.log(process.env.DB_NAME)
 
 export default sequelize
 
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
+
 import sequelize from './config/db/database.js'
 import Model from './app/models/index.js'
-import 'dotenv/config'
 
-console.log(process.env.HOST)
+sequelize.sync()
