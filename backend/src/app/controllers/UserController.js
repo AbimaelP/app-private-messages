eaa0062971c443959b8e01c1f9d558108d195d9d
Abimael Silva Campinas <a.bimael2000@hotmail.com>
2023-04-08 11:52:04 -0300

    Organização do projeto, abreviação pastas e caminhos dispensaveis

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -9,24 +9,21 @@ class UserController {
     }
 
     async create(data){
-        //criação de codigo com 32 caracteres para o token
         const tokenCod = await uuidv4()
-        //cria um usuario, e insere os dados que foram gravados, dentro de uma variavel
         const userCreated = await User.create(data).then((response) => {
             return response
         }).catch((error) => {
             return error
         })
-        //verifica se o usuario foi criado com sucesso, se não, retorna um erro
+        
         if(userCreated.errors){
             return {
                 codRequest: 401,
                 error: 'Esse email ja está cadastrado'
             }
         }
-        //cria um token utilizando o ID do usuario que foi criado como chave estrangeira
+        
         const token = await Token.create({ token_cod: tokenCod, UserId: userCreated.id })
-        //retorna a resposta para a rota que chamou o método
         return {
             codRequest: 200,
             token_cod: token.token_cod
